cf688f44c13552f8f7515653c6bbded1855821e1
Ariel <arielgch@gmail.com>
2021-03-06 23:47:59 -0300

    a

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["template"],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
